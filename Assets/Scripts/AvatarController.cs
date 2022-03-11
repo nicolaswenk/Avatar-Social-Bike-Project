755bca88feff751847a34e43e76e241588cd8750
nicolaswenk <nicolas.wenk@gmail.com>
2022-03-11 14:46:21 +0100

    Raw IK position animation for hands, feet, and look
    Missing: rotational animation for feet and hands; neck rotation to look forward; proxy game objects/shifts to improve IK goals; rotating pedals' end

---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+    public GameObject seat;
+    public PedalRotator pedals;
+    private Animator animator;
+
+    public GameObject rightHandle;
+    public GameObject leftHandle;
+
+    public GameObject steer;
+
+    [Range(0, 2)]
+    public float bentOver=0.5f;
+
+    private Quaternion initialRotation;
+
+    void Start()
+    {
+        initialRotation = this.transform.localRotation;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        this.transform.position=seat.transform.position;
+    }
+
+    async void OnAnimatorIK(int layerIndex)
+    {
+
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
+
+        animator.SetIKPosition(AvatarIKGoal.LeftFoot, pedals.leftEnd.transform.position);
+        animator.SetIKPosition(AvatarIKGoal.RightFoot, pedals.rightEnd.transform.position);
+
+        
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandle.transform.position);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandle.transform.position);
+
+        animator.SetLookAtWeight(1, 1, 0, 1);
+        animator.SetLookAtPosition(this.transform.position+steer.transform.right * bentOver);
+
+    }
+
+    void LateUpdate()
+    {
+        //TODO: Animate the neck to look forward
+    }
+}
---
+++ Assets/Scripts/PedalRotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedalRotator : MonoBehaviour
+{
+    public GameObject leftEnd;
+    public GameObject rightEnd;
+
+    public float tourProgress=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tourProgress = Time.time/5.0f;
+
+        this.transform.localRotation = Quaternion.Euler(0,0,-tourProgress*360);
+    }
+}
