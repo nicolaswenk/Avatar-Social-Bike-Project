cf9e9fc6b8dd973d9108bb3efdc8a75f4bc0b947
nicolaswenk <nicolas.wenk@gmail.com>
2022-03-11 16:31:11 +0100

    Apply to woman avatar with proxies
    Feet rotation totally wrong

--- Assets/Scripts/AvatarController.cs
+++ Assets/Scripts/AvatarController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class AvatarController : MonoBehaviour
 {
     public GameObject seat;
-    public PedalRotator pedals;
+    public GameObject leftPedal;
+    public GameObject rightPedal;
     private Animator animator;
 
     public GameObject rightHandle;
@@ -35,15 +36,19 @@ public class AvatarController : MonoBehaviour
         animator.SetIKPositionWeight(AvatarIKGoal.LeftFoot, 1);
         animator.SetIKPositionWeight(AvatarIKGoal.RightFoot, 1);
 
-        animator.SetIKPosition(AvatarIKGoal.LeftFoot, pedals.leftEnd.transform.position);
-        animator.SetIKPosition(AvatarIKGoal.RightFoot, pedals.rightEnd.transform.position);
+        animator.SetIKPosition(AvatarIKGoal.LeftFoot, leftPedal.transform.position);
+        animator.SetIKPosition(AvatarIKGoal.RightFoot, rightPedal.transform.position);
 
         
         animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
         animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
 
         animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandle.transform.position);
         animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandle.transform.position);
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandle.transform.rotation);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandle.transform.rotation);
 
         animator.SetLookAtWeight(1, 1, 0, 1);
         animator.SetLookAtPosition(this.transform.position+steer.transform.right * bentOver);
---
+++ Assets/Scripts/RotationGoal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationGoal : MonoBehaviour
+{
+    public Transform rotationGoal;
+
+    private Quaternion initialRotation;
+    private Quaternion goalInitialRotation;
+
+    private void Start()
+    {
+        initialRotation = this.transform.rotation;
+        goalInitialRotation = this.rotationGoal.rotation;
+    }
+
+    void Update(){
+        this.transform.rotation = initialRotation * Quaternion.Inverse(goalInitialRotation) * rotationGoal.rotation;
+    }
+}
